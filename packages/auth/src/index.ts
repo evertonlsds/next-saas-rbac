bec9dfb9ea7414077e2f01e3c44a2d4d1be7de9a
evertonlsds <everton0102@gmail.com>
2024-09-25 14:02:25 -0300

    cong api folder

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,11 +1,12 @@
 import {
+  AbilityBuilder,
   CreateAbility,
   createMongoAbility,
   ForcedSubject,
   MongoAbility,
 } from '@casl/ability'
 
-const actions = ['manage', 'invite'] as const
+const actions = ['manage', 'invite', 'delete'] as const
 const subjects = ['User', 'all'] as const
 type AppAbilities = [
   (typeof actions)[number],
@@ -17,3 +18,10 @@ type AppAbilities = [
 
 export type AppAbility = MongoAbility<AppAbilities>
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
+
+const { build, can, cannot } = new AbilityBuilder(createAppAbility)
+
+can('invite', 'User')
+cannot('delete', 'User')
+
+export const ability = build()
