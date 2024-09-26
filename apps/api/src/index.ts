4ea1abe0e956d0633aee39a885b81172dd9c1820
evertonlsds <everton0102@gmail.com>
2024-09-26 16:23:09 -0300

    roles permission configuration

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,1 +1,8 @@
-import { ability } from '@saas/auth'
+import { defineAbilityFor } from '@saas/auth'
+
+const ability = defineAbilityFor({ role: 'ADMIN' })
+
+const userCanInviteSOmeoneElse = ability.can('invite', 'User')
+const userCanDeleteOtherUsers = ability.can('delete', 'User')
+
+const userCannotDeleteOtherUsers = ability.cannot('delete', 'User')
